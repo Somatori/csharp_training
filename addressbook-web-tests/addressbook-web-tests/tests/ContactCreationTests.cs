06dc23272337b19e4a0a418adeeb25cd35d28e8b
Иван Иванов <venustype@outlook.com>
2017-06-02 16:01:09 +0300

    Module #2.4. Errors in tests

---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests : TestBase
+    {
+        [Test]
+        public void ContactCreationTest()
+        {
+            ContactData contact = new ContactData("John", "Johnson");
+            app.Contacts.Create(contact);
+            app.Auth.Logout();
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            GroupData groop = new GroupData("aaa");
+            groop.Header = "ddd";
+            groop.Footer = "fff";
+
+            app.Groups.Create(groop);
+            app.Auth.Logout();
+        }
+
+        [Test]
+        public void EmptyGroupCreationTest()
+        {
+            GroupData groop = new GroupData("");
+            groop.Header = "";
+            groop.Footer = "";
+
+            app.Groups.Create(groop);
+            app.Auth.Logout();
+        }
+    }
+}
