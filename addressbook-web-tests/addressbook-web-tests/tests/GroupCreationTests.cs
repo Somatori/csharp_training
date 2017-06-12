f666014031c3d9618440449fcc7b937211c6e996
Иван Иванов <venustype@outlook.com>
2017-06-12 10:52:11 +0300

    - The 'GetGroupList()' method is added to 'GroupHelper' class; - Verifications are added to existing group creation tests; - The 'BadNameGroupCreationTest()' test is added.

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -12,23 +13,46 @@ namespace WebAddressbookTests
         [Test]
         public void GroupCreationTest()
         {
-            GroupData groop = new GroupData("aaa");
-            groop.Header = "ddd";
-            groop.Footer = "fff";
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "fff";
 
-            app.Groups.Create(groop);
-            // app.Auth.Logout();
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();            
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
         }
 
         [Test]
         public void EmptyGroupCreationTest()
         {
-            GroupData groop = new GroupData("");
-            groop.Header = "";
-            groop.Footer = "";
+            GroupData group = new GroupData("");
+            group.Header = "";
+            group.Footer = "";
 
-            app.Groups.Create(groop);
-            // app.Auth.Logout();
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+        }
+
+        [Test]
+        public void BadNameGroupCreationTest()
+        {
+            GroupData group = new GroupData("a'a");
+            group.Header = "";
+            group.Footer = "";
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
         }
     }
 }
